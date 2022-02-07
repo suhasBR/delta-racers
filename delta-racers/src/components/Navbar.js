b7f097fa74c50a4b1e206484ed8f95bc613c031d
suhasbr <suhas.bangalore16@gmail.com>
2022-02-07 06:49:52 +0530

    submission commit

--- delta-racers/src/components/Navbar.js
+++ delta-racers/src/components/Navbar.js
@@ -1,60 +1,74 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from "react";
 import styles from "./Navbar.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { connectWallet } from "../actions/connect";
 
 function Navbar() {
+  const isConnected = useSelector((state) => state.user.loggedIn);
+  const addr = useSelector((state) => state.user.address);
+  const loading = useSelector((state) => state.user.loading);
 
-    const isConnected = useSelector((state) => state.user.loggedIn);
-    const addr = useSelector((state) => state.user.address);
-    const loading = useSelector((state) => state.user.loading);
-  
-    const connectWalletHandler =  () => {
-      connectWallet();
-    };
+  const connectWalletHandler = () => {
+    connectWallet();
+  };
 
-  return <div className={styles["navbar-full"]}>
+  return (
+    <div className={styles["navbar-full"]}>
       <div className={styles["logo-container"]}>
-          <Link to="/">
-          <img src="/images/logo.svg" 
-          className={styles["logo-img"]}
-          alt="logo"/>
-          </Link>
+        <Link to="/">
+          <img
+            src="/images/logo.svg"
+            className={styles["logo-img"]}
+            alt="logo"
+          />
+        </Link>
       </div>
       <div className={styles["connect-button-holder"]}>
-      {!isConnected ? (
-       <Fragment>
-         {
-           loading ? (
-            <button
-            onClick={connectWalletHandler}
-            className={styles["connect-button"]}
-          >
-            ...
-          </button>
-           ): (
-            <button
-            onClick={connectWalletHandler}
-            className={styles["connect-button"]}
-          >
-            Connect Wallet
-          </button>
-           )
-         }
-         
+        {isConnected && (
+          <div className={styles["navbar-titles"]}>
+            <Link to="/races">
+              <h4 className={styles["navbar-title"]}>Race</h4>
+            </Link>
+
+            <Link to="/components">
+              <h4 className={styles["navbar-title"]}>Buy</h4>
+            </Link>
+
+            <Link to="/lobby">
+              <h4 className={styles["navbar-title"]}>Lobby</h4>
+            </Link>
+          </div>
+        )}
+        {!isConnected ? (
+          <Fragment>
+            {loading ? (
+              <button
+                onClick={connectWalletHandler}
+                className={styles["connect-button"]}
+              >
+                ...
+              </button>
+            ) : (
+              <button
+                onClick={connectWalletHandler}
+                className={styles["connect-button"]}
+              >
+                Connect Wallet
+              </button>
+            )}
           </Fragment>
         ) : (
           <button
             onClick={connectWalletHandler}
             className={styles["connect-button"]}
           >
-            Connected to {addr.substring(0,5)+'....'}
+            Connected to {addr.substring(0, 5) + "...."}
           </button>
         )}
       </div>
-
-  </div>;
+    </div>
+  );
 }
 
 export default Navbar;
